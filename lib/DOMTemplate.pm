1a3d7431be665dad1f4b8f604e84fd3b3590b623
awwaiid <awwaiid@thelackthereof.org>
2007-08-27 08:43:29 -0400

    Remove debugging from the DOMTemplate
    darcs-hash:20070827124329-4fc8e-74b7ae443155dc5fa765dffaa325360b50c9c4bc.gz

--- lib/DOMTemplate.pm
+++ lib/DOMTemplate.pm
@@ -74,16 +74,12 @@ sub get_classes {
 sub set {
   my ($self, %attrs) = @_;
   foreach my $key (keys %attrs) {
-    print STDERR "DOM SET: $key\n";
     my @nodes = $self->find_by_selector($key);
-    print STDERR "Found " . (scalar @nodes) . " nodes!\n";
     foreach my $node (@nodes) {
       my $tag = $node->tag;
       if($tag eq 'input' || $tag eq 'option') {
-        print STDERR "Found $tag, so setting value.\n";
         $node->attr(value => $attrs{$key});
       } else {
-        print STDERR "Setting content.\n";
         $node->replace_content(to_node($attrs{$key}));
       }
     }
@@ -110,13 +106,10 @@ sub set_by_selector {
 sub set_value_by_selector {
   my ($self, %attrs) = @_;
 #  use Data::Dumper;
-#  print STDERR "GAR2: " . Dumper(\%attrs) . "\n";
   foreach my $key (keys %attrs) {
     my @nodes = $self->find_by_selector($key);
-#  print STDERR "nodes: " . Dumper(\@nodes) . "\n";
     foreach my $node (@nodes) {
       $node->attr(value => $attrs{$key});
-#      print STDERR "SETTING value = $attrs{$key}\n";
     }
   }
 }
@@ -124,7 +117,6 @@ sub set_value_by_selector {
 sub set_value {
   my ($self, %attrs) = @_;
   use Data::Dumper;
-#  print STDERR "GAR: " . Dumper(\%attrs) . "\n";
   foreach my $key (keys %attrs) {
     $self->set_value_by_selector($key => $attrs{$key});
   }
@@ -180,7 +172,6 @@ sub prepend_content {
 # Only supports a few things for now
 sub find_by_selector {
   my ($self, $selector) = @_;
-  #print STDERR "find_by_selector : $selector\n";
 
   my $root = $self;
 
@@ -195,7 +186,6 @@ sub find_by_selector {
       my $type = $1;
       my $name = $2;
       $token =~ s/^(#|\.)?(\w+)//; # Kill this part
-      #print STDERR "find_by_selector type: $type $name ($token)\n";
       if($type eq '') {
         # Tag
         if($root) {
@@ -207,9 +197,7 @@ sub find_by_selector {
       } elsif($type eq '#') {
         # ID
         if($root) {
-          #print STDERR "ROOT: " . $root->as_HTML . "\n\n";
           @ok_nodes = $root->look_down('id', $name);
-          #print STDERR "find_by_selector ok_nodes: @ok_nodes\n";
           undef $root;
         } else {
           @ok_nodes = map { $_->look_down('id', $name) } @ok_nodes;
