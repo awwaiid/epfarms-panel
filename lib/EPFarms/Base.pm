e850cd06d5776671bf0aae49ad020a074264db8f
awwaiid <awwaiid@thelackthereof.org>
2007-08-30 00:27:08 -0400

    Add a cool accessor definition thingie that isn't used yet
    darcs-hash:20070830042708-4fc8e-fcda61e68f9f7fde1130428aec4bbb3355101572.gz

--- lib/EPFarms/Base.pm
+++ lib/EPFarms/Base.pm
@@ -11,6 +11,14 @@ sub new {
   return $self;
 }
 
+sub define_accessors {
+  my ($class, @names) = @_;
+  no strict qw/refs/;
+  foreach my $name (@names) {
+    *{$class.'::'.$name} = sub { $_[0]->{$name} = $_[1] if $_[1]; $_[0]->{$name}};
+  }
+}
+
 
 1;
 
