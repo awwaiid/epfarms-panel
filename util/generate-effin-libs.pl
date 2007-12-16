c1b96ff4fa02d4e99a786f682ff8b73f7cc57f07
awwaiid <awwaiid@thelackthereof.org>
2007-12-16 17:25:25 -0500

    I guess we shouldn't leave my local DB root password in there...
    darcs-hash:20071216222525-4fc8e-517fe22b6c5ff1bc7ecd6c375f16aa90de752804.gz

--- util/generate-effin-libs.pl
+++ util/generate-effin-libs.pl
@@ -5,6 +5,10 @@ use base 'DBIx::Class::Schema::Loader';
 
 package main;
 
+print "Enter password: ";
+my $passwd = <>;
+chomp $password;
+
 EPFarms::DB->dump_to_dir('../lib');
-EPFarms::DB->connection('dbi:mysql:database=epfarms', 'root', 'sulipafude');
+EPFarms::DB->connection('dbi:mysql:database=epfarms', 'root', $passwd);
 
