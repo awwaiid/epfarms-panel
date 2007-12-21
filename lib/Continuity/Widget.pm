363a576febab60f609105cefed66b3493b071365
awwaiid <awwaiid@thelackthereof.org>
2007-12-21 16:25:42 -0500

    Rename some fields
    darcs-hash:20071221212542-4fc8e-68da469a4ac31cbc532ac51155ec79b789b7e5a0.gz

--- lib/Continuity/Widget.pm
+++ lib/Continuity/Widget.pm
@@ -5,15 +5,14 @@ use Moose;
 use Coro::Continuation;
 use Data::UUID;
 
-has 'uuid' => (
+has 'cont' => (is => 'rw');
+has 'request' => (is => 'rw');
+has 'id' => (
   is      => 'ro', 
   isa     => 'Str', 
   default => sub { Data::UUID->new->create_str }
 );
-
 has 'output' => (is => 'rw',   isa => 'Str', default => '');
-has 'cont' => (is => 'rw');
-has 'request' => (is => 'rw');
 
 sub process {
   my ($self) = @_;
