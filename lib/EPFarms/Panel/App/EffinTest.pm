88e84ae619b2c74a2c9f6dec8a55ffb5a0cf083d
awwaiid <awwaiid@thelackthereof.org>
2008-01-16 17:22:53 -0500

    Move EffinTest closer to being AccountingAdmin
    darcs-hash:20080116222253-4fc8e-dbcd93015b7b2030d3ffec8c8a69141f6733c795.gz

--- lib/EPFarms/Panel/App/EffinTest.pm
+++ lib/EPFarms/Panel/App/EffinTest.pm
@@ -2,6 +2,7 @@ package EPFarms::Panel::App::EffinTest;
 
 use Moose;
 extends 'EPFarms::Panel::App';
+use EPFarms::Effin;
 
 has '+config' => (default => sub {{
   rank => '99',
@@ -11,26 +12,110 @@ has '+config' => (default => sub {{
   user_group => 'paneldev',
 }});
 
-use EPFarms::Effin;
+has callback => (is => 'rw', default => sub{{}});
 
-sub main {
-  my $self = shift;
+sub get_mysql_auth {
+  my ($self) = @_;
+  my $dsn = 'DBI:mysql:database=epfarms_effin_effin';
   my $username = $self->{panel}->{user}->{username};
   my $password = $self->{panel}->{user}->{password};
+
+  # First try with what we have
+  eval { DBI->connect($dsn, $username, $password, { RaiseError => 1}) };
+  return ($username, $password) unless $@;
+
+  # Then ask for the password
+  $self->display(qq|
+    <p>Our default entry for your MySQL username and password failed. Please enter your MySQL username and password. This will be automated in the near future.</p>
+    <br><br>
+    Username: <input type=text name=username value="$username"><br>
+    Password: <input type=password name=password><br>
+    <input type=submit>
+  |);
+  my $username = $self->param('username');
+  my $password = $self->param('password');
+  print STDERR "Trying MySQL User: $username\n";
+
+  eval { DBI->connect($dsn, $username, $password, { RaiseError => 1}) };
+  return ($username, $password) unless $@;
+
+  $self->display(qq|
+    <p>Well that didn't work either. I'm letting support know that there is a problem and they will contact you. You can try again too, if you like.</p>
+  |);
+  return;
+
+}
+
+sub add_link {
+  my ($self, $text, $subref) = @_;
+  my $name = scalar $subref;
+  $name =~ s/CODE\(0x(.*)\)/$1/;
+  $self->callback->{$name} = $subref;
+  return qq{<a href="?callback=$name">$text</a>};
+}
+
+sub process_links {
+  my ($self) = @_;
+  my $name = $self->param('callback');
+  if($self->callback->{$name}) {
+    $self->callback->{$name}->();
+    $self->callback({});
+    return 1;
+  }
+  # Reset callback hash
+  $self->callback({});
+  return 0;
+}
+
+sub main {
+  my ($self) = @_;
+  my ($username, $password) = $self->get_mysql_auth;
+  return unless $username;
   my $db = EPFarms::Effin->connect(
     'dbi:mysql:database=epfarms_effin_effin',
     $username,
     $password
   );
-  my $out = '';
-  my @ok_users = split(' ', (getgrnam('paneldev'))[3]);
-  $out .= "paneldev users: @ok_users<br>";
+
+  do {
+    my $out = '';
+
+    $out .= "<table class=data>
+      <thead>
+        <tr class=header>
+          <th>User</th>
+          <th>Balance</th>
+        </tr>
+      </thead>";
+    foreach my $user ($db->resultset('Users')->all) {
+      $out .= "<tr><td>"
+        . $self->add_link($user->usr_nname => sub { $self->user_detail($user) })
+        . "</td>"
+        . "<td>"
+        . $user->balance_formatted
+        . "</td></tr>";
+    }
+    $out .= "</table>";
+
+    $self->display($out);
+  } while $self->process_links;
+}
+
+sub add_transaction_for {
+  my ($self, $user) = @_;
+  $self->display("Hello! Examining user: " . $user->usr_nname);
+}
 
 
-  foreach my $user ($db->resultset('Users')->all) {
-    $out .= "<h3>" . $user->usr_nname . "</h3>\n";
+sub user_detail {
+  my ($self, $user) = @_;
+  do {
+    my $out = qq|
+      <h2>User: @{[$user->usr_nname]}</h2>
+      Balance: @{[$user->balance_formatted]}<br>
+    |;
     if(scalar ($user->transactions) > 0) {
-      $out .= q{
+      $out .= qq|
         <h4>Transactions</h4>
         <table border=1 cellspacing=0 cellpadding=2 width="100%">
           <tr>
@@ -38,19 +123,22 @@ sub main {
             <th>Description</th>
             <th>Amount</th>
           </tr>
-      };
+      |;
       foreach my $transaction ($user->transactions) {
-        $out .= "<tr>"
-          . "<td>" . $transaction->trn_date . "</td>\n"
-          . "<td>" . $transaction->trn_memo . "</td>\n"
-          . "<td>" . $transaction->trn_amount . "</td>\n"
-          . "</tr>\n";
+        $out .= qq|
+          <tr>
+            <td>@{[$transaction->trn_date]}</td>
+            <td>@{[$transaction->trn_memo]}</td>
+            <td>@{[$transaction->trn_amount]}</td>
+          </tr>
+        |;
       }
       $out .= "</table>";
     }
-  }
-  $self->display($out);
+    $self->display($out);
+  } while $self->process_links;
 }
+
 
 1;
 
