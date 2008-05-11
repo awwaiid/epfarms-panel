bcc9e1e96fdaf02d12a00d776b73dbafd5f7e9b1
awwaiid <awwaiid@thelackthereof.org>
2008-05-11 02:24:36 -0400

    Remove unneeded component load
    darcs-hash:20080511062436-4fc8e-4ab45ef3a05d5433304d82df5ecca1b9f858d807.gz

--- lib/EPFarms/Effin/MyTransactions.pm
+++ lib/EPFarms/Effin/MyTransactions.pm
@@ -70,7 +70,5 @@ __PACKAGE__->add_columns(
 # Created by DBIx::Class::Schema::Loader v0.04005 @ 2008-05-10 21:30:48
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:DLXNODVfw3CeMppNXGIHIQ
 
-__PACKAGE__->load_components("InflateColumn::DateTime", "Core");
-
 # You can replace this text with custom content, and it will be preserved on regeneration
 1;
