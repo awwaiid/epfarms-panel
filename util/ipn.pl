3affdd347d1671f64f426b1760d3d51a846f0ca5
mburns.lungching <mburns.lungching@gmail.com>
2009-10-22 18:51:12 -0400

    make ipn.pl testable
    darcs-hash:20091022225112-26b3a-f6e54e169aa088193e951a308a156b5244f9389b.gz

--- util/ipn.pl
+++ util/ipn.pl
@@ -1,37 +1,94 @@
 #!/usr/bin/perl
 
-use lib '/usr/local/lib/EPFarms-Panel/lib';
+use lib 'lib';
 use Data::Dumper;
-use EPFarms::DB;
+use EPFarms;
+use EPFarms::Transaction;
+use EPFarms::User;
+use strict;
 
-# read post from PayPal system and add 'cmd'
-read (STDIN, $query, $ENV{'CONTENT_LENGTH'});
-$query .= '&cmd=_notify-validate';
+my $query;
+my %variable;
 
-# post back to PayPal system to validate
-use LWP::UserAgent;
-$ua = new LWP::UserAgent;
-#$req = new HTTP::Request 'POST','https://www.sandbox.paypal.com/cgi-bin/webscr';
-$req = new HTTP::Request 'POST','https://www.paypal.com/cgi-bin/webscr';
+my $DEBUG = shift;
+my ($DEBUG_ERROR, $DEBUG_INVALID, $DEBUG_VERIFIED);
+my ($res_error, $res_content);
 
-$req->content_type('application/x-www-form-urlencoded');
-$req->content($query);
-$res = $ua->request($req);
+if (! $DEBUG) {
+    # read post from PayPal system and add 'cmd'
+    read (STDIN, $query, $ENV{'CONTENT_LENGTH'});
+    $query .= '&cmd=_notify-validate';
 
-# split posted variables into pairs
-@pairs = split(/&/, $query);
-$count = 0;
-foreach $pair (@pairs) {
-  ($name, $value) = split(/=/, $pair);
-  $value =~ tr/+/ /;
-  $value =~ s/%([a-fA-F0-9][a-fA-F0-9])/pack("C", hex($1))/eg;
-  $variable{$name} = $value;
-  $count++;
+    # post back to PayPal system to validate
+    use LWP::UserAgent;
+    my $ua = new LWP::UserAgent;
+    #$req = new HTTP::Request 'POST','https://www.sandbox.paypal.com/cgi-bin/webscr';
+    my $req = new HTTP::Request 'POST','https://www.paypal.com/cgi-bin/webscr';
+
+    $req->content_type('application/x-www-form-urlencoded');
+    $req->content($query);
+    my $res = $ua->request($req);
+    $res_error   = $res->is_error;
+    $res_content = $res->content;
+
+    # split posted variables into pairs
+    my @pairs = split(/&/, $query);
+    foreach my $pair (@pairs) {
+      my ($name, $value) = split(/=/, $pair);
+      $value =~ tr/+/ /;
+      $value =~ s/%([a-fA-F0-9][a-fA-F0-9])/pack("C", hex($1))/eg;
+      $variable{$name} = $value;
+    }
+}
+else {
+    %variable = (
+          'verify_sign' => 'An5ns1Kso7MWUdW4ErQKJJJ4qi4-A-y.fomnpzBGn79P6lAAVO0zOmXu',
+          'payer_id' => 'DWG7XF6LZTQD4',
+          'residence_country' => 'US',
+          'address_state' => 'DC',
+          'receiver_email' => 'epfarms@epfarms.org',
+          'address_status' => 'confirmed',
+          'payment_type' => 'instant',
+          'address_city' => 'Washington',
+          'business' => 'epfarms@epfarms.org',
+          'address_street' => '2130 P Street NW #509',
+          'shipping' => '0.00',
+          'payment_status' => 'Completed',
+          'cmd' => '_notify-validate',
+          'txn_type' => 'web_accept',
+          'payment_fee' => '0.45',
+          'address_country' => 'United States',
+          'charset' => 'windows-1252',
+          'payment_date' => '20:54:18 Aug 22, 2007 PDT',
+          'option_selection1' => '',
+          'receipt_id' => '2934-2022-5570-8595',
+          'mc_fee' => '0.45',
+          'payer_status' => 'unverified',
+          'address_zip' => '20037',
+          'custom' => '',
+          'payment_gross' => '5.00',
+          'txn_id' => '3X583446M01797847',
+          'last_name' => 'Wilcox',
+          'receiver_id' => 'DWN2DXXXYC82W',
+          'item_number' => '',
+          'address_country_code' => 'US',
+          'payer_email' => 'awwaiid@thelackthereof.org',
+          'quantity' => '1',
+          'address_name' => 'Brock Wilcox',
+          'tax' => '0.00',
+          'item_name' => 'EPFarms Account Deposit',
+          'notify_version' => '2.4',
+          'mc_gross' => '5.00',
+          'option_name1' => 'awwaiid',
+          'mc_currency' => 'USD',
+          'first_name' => 'Brock'
+    );
+    $DEBUG_VERIFIED = 1;
 }
 
-if ($res->is_error) {
-  # HTTP error
-} elsif ($res->content eq 'VERIFIED') {
+if ($res_error || $DEBUG_ERROR) {
+  # TODO - Log error
+} elsif ($res_content eq 'VERIFIED' || $DEBUG_VERIFIED) {
   # check the payment_status=Completed
   # check that txn_id has not been previously processed
   # check that receiver_email is an email address in your PayPal account
@@ -83,7 +140,7 @@ if ($res->is_error) {
 
   }
 
-} elsif ($res->content eq 'INVALID') {
+} elsif ($res_content eq 'INVALID' || $DEBUG_INVALID) {
   # log for manual investigation
   # print to screen the following if the IPN POST was INVALID
 
