8659c36d81e9c4e8265e990bec236f6bb1618949
awwaiid <awwaiid@thelackthereof.org>
2010-01-10 20:16:21 -0500

    Update DomainRegistration to use MooseX::Declare, and add index
    darcs-hash:20100111011621-4fc8e-abe1c7a9f0e8711da48592d25b106bb42b8473f2.gz

--- lib/EPFarms/Service/DomainRegistration.pm
+++ lib/EPFarms/Service/DomainRegistration.pm
@@ -1,25 +1,32 @@
-package EPFarms::Service::DomainRegistration;
+class EPFarms::Service::DomainRegistration extends EPFarms::Service {
 
-use Moose;
-extends 'EPFarms::Service';
+  has '+service_name' => ( default => 'Domain Registration' );
+  has '+bill_period' => ( default => 'ondemand' );
+  has '+bill_amount' => ( default => -8.5 );
 
-has '+service_name' => ( default => 'Domain Registration' );
-has '+bill_period' => ( default => 'year' );
-has '+bill_amount' => ( default => -8.5 );
+  has domain_name => (
+    is => 'rw',
+    isa => 'Str',
+    required => 1,
+    description => {
+      label => 'The Real Domain Name'
+    }
+  );
 
-has domain_name => (
-  is => 'rw',
-  isa => 'Str',
-  required => 1,
-  description => {
-    label => 'The Real Domain Name'
+  has auto_renew => (
+    is => 'rw',
+    isa => 'Bool',
+    default => 0
+  );
+  
+  sub index {
+    my ($self) = @_;
+    return {
+      domain_name => $self->domain_name,
+    };
   }
-);
 
-has auto_renew => (
-  is => 'rw',
-  isa => 'Bool',
-  default => 0
-);
+}
 
 1;
+
